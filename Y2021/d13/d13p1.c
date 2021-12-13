fb7e99c2975b6b1d4e0e1d426aca0b267ebf8067
daduam <jkampadu002@st.ug.edu.gh>
2021-12-13 17:26:15 +0000

    add complete day 13

---
+++ Y2021/d13/d13p1.c
@@ -0,0 +1,41 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+
+#define DIM 1500
+int grid[DIM][DIM];
+void fold_along(char axis, int d) {
+    int i, j;
+    if (axis == 'x') {
+        for (i = 0; i < DIM; i++) {
+            for (j = 0; j < d; j++) {
+                grid[i][j] = grid[i][j] || grid[i][2*d-j];
+                grid[i][2*d-j] = 0;
+            }
+        }
+    } else {
+        for (i = 0; i < d; i++) {
+            for (j = 0; j < DIM; j++) {
+                grid[i][j] = grid[i][j] || grid[2*d-i][j];
+                grid[2*d-i][j] = 0;
+            }
+        }
+    }
+}
+
+int main() {
+    int x, y, d, i, j;
+    char axis;
+    while (scanf("%d,%d", &y, &x) == 2) {
+        grid[x][y] = 1;
+    }
+    scanf("fold along %c=%d", &axis, &d);
+    fold_along(axis, d);
+    int count = 0;
+    for (i = 0; i < DIM; i++) {
+        for (j = 0; j < DIM; j++) {
+            count += grid[i][j];
+        }
+    }
+    printf("Dots after first fold: %d\n", count);
+}
---
+++ Y2021/d13/d13p2.c
@@ -0,0 +1,45 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+
+#define DIM 1500
+int grid[DIM][DIM];
+void fold_along(char axis, int d) {
+    int i, j;
+    if (axis == 'x') {
+        for (i = 0; i < DIM; i++) {
+            for (j = 0; j < d; j++) {
+                grid[i][j] = grid[i][j] || grid[i][2*d-j];
+                grid[i][2*d-j] = 0;
+            }
+        }
+    } else {
+        for (i = 0; i < d; i++) {
+            for (j = 0; j < DIM; j++) {
+                grid[i][j] = grid[i][j] || grid[2*d-i][j];
+                grid[2*d-i][j] = 0;
+            }
+        }
+    }
+}
+
+int main() {
+    int x, y, d, i, j;
+    char axis;
+    while (scanf("%d,%d", &y, &x) == 2) {
+        grid[x][y] = 1;
+    }
+    while (1) {
+        scanf("fold along %c=%d%*c", &axis, &d);
+        if (feof(stdin)) {
+            break;
+        }
+        fold_along(axis, d);
+    }
+    for (i = 0; i < 10; i++) {
+        for (j = 0; j < 50; j++) {
+            printf("%c", grid[i][j] ? '#' : ' ');
+        }
+        puts("");
+    }
+}
